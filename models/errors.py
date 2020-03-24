0ef271508d6bd9692825812a350522b247683644
Jonathan Decker <Discord@hotmailde>
2020-03-24 02:48:19 +0100

    Added errors for command interpreter Added command field to query

--- models/errors.py
+++ models/errors.py
@@ -5,3 +5,17 @@ class TokenLoadingError(Exception):
     :description: Raised when load_token is unable to load a token
     """
     pass
+
+
+class MainCommandTypeError(Exception):
+    """
+    :description: Raised when trying to assign a non-existent main command type.
+    """
+    pass
+
+
+class SubCommandTypeError(Exception):
+    """
+    :description: Raised when trying to assign a non-existent sub command type.
+    """
+    pass
--- models/query.py
+++ models/query.py
@@ -5,6 +5,7 @@
 from dataclasses import dataclass
 import logging
 from discord import Message
+from frontend.command_interpreter import CommandType
 
 logger = logging.getLogger('pb_logger')
 
@@ -16,19 +17,20 @@ class Query:
     Should mainly be created by ingoing interfaces.
     While processing the query the fields should be further filled out and updated.
     """
-    command: str
+    raw_command: str
     context_type: str
     discord_channel: Message.channel
     forward_to: str
     output_message: str
+    # TODO add Payload to transport data from backend to frontend
 
-    def __init__(self, command: str, context_type: str, discord_channel: Message.channel = None,
-                 forward_to: str = "frontend", output_message: str = ""):
+    def __init__(self, raw_command: str, context_type: str, discord_channel: Message.channel = None,
+                 forward_to: str = "frontend", output_message: str = "", command: CommandType = None):
         """
         :description: For new Query Objects its assumed they originate from some interface
          and should be forwarded to the frontend.
-        :param command: The actual command issued by the user.
-        :type command: str
+        :param raw_command: The actual command issued by the user.
+        :type raw_command: str
         :param context_type: The type of context the command was issued or should go back to e.g. discord, cli
         :type context_type: str
         :param discord_channel: In case the command comes from a discord chat interface,
@@ -39,7 +41,7 @@ class Query:
         :param output_message: The message that should be sent back to the user via the channel.
         :type output_message: str
         """
-        self.command = command
+        self.raw_command = raw_command
         self.context_type = context_type
         self.discord_channel = discord_channel
         self.forward_to = forward_to
@@ -51,4 +53,4 @@ class Query:
         :return: Returns the command followed by the context_type fields for now.
         :rtype: str
         """
-        return str(f"{self.command}, {self.context_type}")
+        return str(f"{self.raw_command}, {self.context_type}")
