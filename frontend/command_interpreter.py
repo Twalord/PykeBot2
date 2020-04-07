5d5b291c25004e1d2e77cb9446b0cc63cb412f7d
Jonathan Decker <Discord@hotmailde>
2020-04-07 16:21:19 +0200

    Implemented command_interpreter.py

--- frontend/command_interpreter.py
+++ frontend/command_interpreter.py
@@ -1,40 +1,70 @@
-from models.query import Query
-from dataclasses import dataclass
-from models.errors import MainCommandTypeError, SubCommandTypeError
-import logging
+"""
 
+:author: Jonathan Decker
+"""
+from models.query import Query
+import logging
+from models.lookup_tables import stalk_command_lookup, all_flags_lookup
+from models.data_models import Error
 
 logger = logging.getLogger("pb_logger")
 
-db_query_types = ["Team", "Player", "Tournament", "Date", "Time_Frame"]
-new_info_types = ["Tournament_Link", "Alias"]
-refresh_types = ["Team", "Player", "Tournament", "Tournament_Link"]
-settings_types = ["Update", "Check"]
-other_types = ["Other"]
-
-
-command_types = {"db_query": db_query_types,
-                 "new_info": new_info_types,
-                 "refresh": refresh_types,
-                 "settings": settings_types,
-                 "other": other_types}
-
-
-@dataclass
-class CommandType:
-
-    main_type: str
-    sub_type: str
-
-    def __init__(self, main_type: str, sub_type: str):
-        if main_type not in command_types.keys():
-            raise MainCommandTypeError
-        if sub_type not in command_types.get(main_type):
-            raise SubCommandTypeError
-
-        self.main_type = main_type
-        self.sub_type = sub_type
-
 
 def interpret_command(query: Query):
-    pass
+
+    flags = set()
+    url = ""
+    # fetch raw command
+    raw_commands = query.raw_command.split()
+
+    # to drop the .pb at the beginning
+    raw_commands.pop()
+
+    if len(raw_commands) == 0:
+        error_message = f"Command {str(query)} failed, as no command was provided."
+        logger.error(error_message)
+        create_error(query, error_message)
+        return
+
+    base_command = raw_commands[0]
+
+    if base_command in stalk_command_lookup:
+
+        # extract data (e.g. url)
+        # error case as stalk command needs data
+        if len(raw_commands) == 1:
+            error_message = f"Command {str(query)} failed, as no data for stalking was provided."
+            logger.error(error_message)
+            create_error(query, error_message)
+            return
+
+        # check for flags
+        elif len(raw_commands) > 2:
+            url = raw_commands[len(raw_commands)-1]
+            # everything that is not the base command or the data in the last position is considered a flag
+            flags = {*raw_commands[1:-1]}
+
+            # check for invalid flags
+            for flag in flags:
+                if flag not in all_flags_lookup:
+                    error_message = f"Command {str(query)} failed, as an unknown flag {flag} was provided."
+                    logger.error(error_message)
+                    create_error(query, error_message)
+                    return
+
+        else:
+            url = raw_commands[1]
+
+        # update query so it can be forwarded to backend
+        query.update_query("backend", "stalk", data=url, flags=flags)
+        return
+    else:
+        error_message = f"Only basic stalking has been implemented so far, command {str(query)} failed."
+        logger.error(error_message)
+        create_error(query, error_message)
+        return
+
+
+def create_error(query: Query, content: str):
+    error = Error(content)
+    query.update_query("frontend", "format", payload=error)
