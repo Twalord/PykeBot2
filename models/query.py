3e16e7a2b4368834c9dad63a7b1e287620b68eb1
Jonathan Decker <Discord@hotmailde>
2020-04-07 16:17:55 +0200

    Updated Query with new fields, and renamed a few. Also changed the query init order and mandatory fields. Further added a update_query function which may be used to change multiple fields at once while also performing checks on the new values.

--- models/query.py
+++ models/query.py
@@ -5,7 +5,10 @@
 from dataclasses import dataclass
 import logging
 from discord import Message
-from frontend.command_interpreter import CommandType
+from models.data_models import Payload
+from models.lookup_tables import next_step_lookup, forward_to_lookup
+from models.errors import InvalidForwardToError, InvalidNextStepError
+from typing import Set
 
 logger = logging.getLogger('pb_logger')
 
@@ -22,10 +25,14 @@ class Query:
     discord_channel: Message.channel
     forward_to: str
     output_message: str
-    # TODO add Payload to transport data from backend to frontend
+    data: str
+    flags: Set[str]
+    payload: Payload
+    next_step: str
 
-    def __init__(self, raw_command: str, context_type: str, discord_channel: Message.channel = None,
-                 forward_to: str = "frontend", output_message: str = "", command: CommandType = None):
+    def __init__(self, context_type: str, forward_to: str, next_step: str, raw_command: str = "",
+                 discord_channel: Message.channel = None, data: str = "", output_message: str = "",
+                 flags: Set[str] = None, payload: Payload = None):
         """
         :description: For new Query Objects its assumed they originate from some interface
          and should be forwarded to the frontend.
@@ -41,11 +48,23 @@ class Query:
         :param output_message: The message that should be sent back to the user via the channel.
         :type output_message: str
         """
+        if forward_to not in forward_to_lookup:
+            logger.error(f"Failed to create query as forward_to could not be matched: {forward_to}")
+            raise InvalidForwardToError
+
+        if next_step not in next_step_lookup:
+            logger.error(f"Failed to create query as next_step could not be matched: {next_step}")
+            raise InvalidNextStepError
+
         self.raw_command = raw_command
         self.context_type = context_type
         self.discord_channel = discord_channel
-        self.forward_to = forward_to
         self.output_message = output_message
+        self.payload = payload
+        self.data = data
+        self.flags = flags
+        self.forward_to = forward_to
+        self.next_step = next_step
 
     def __str__(self):
         """
@@ -54,3 +73,42 @@ class Query:
         :rtype: str
         """
         return str(f"{self.raw_command}, {self.context_type}")
+
+    def update_query(self, forward_to, next_step, data: str = None, flags: Set[str] = None, output_message: str = None, payload: Payload = None):
+        """
+
+        :param forward_to:
+        :type forward_to:
+        :param next_step:
+        :type next_step:
+        :param data:
+        :type data:
+        :param flags:
+        :type flags:
+        :param output_message:
+        :type output_message:
+        :param payload:
+        :type payload:
+        :return:
+        :rtype:
+        """
+        if forward_to not in forward_to_lookup:
+            logger.error(f"Failed to create query as forward_to could not be matched: {forward_to}")
+            raise InvalidForwardToError
+
+        if next_step not in next_step_lookup:
+            logger.error(f"Failed to create query as next_step could not be matched: {next_step}")
+            raise InvalidNextStepError
+
+        self.forward_to = forward_to
+        self.next_step = next_step
+        self.data = data
+        if flags is not None:
+            if self.flags is not None:
+                self.flags.union(flags)
+            else:
+                self.flags = flags
+        if output_message is not None:
+            self.output_message = output_message
+        if payload is not None:
+            self.payload = payload
