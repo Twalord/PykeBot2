c24a6ac94454806bb0eb250d4f7a34a60d2348ce
Jonathan Decker <Discord@hotmailde>
2020-04-07 16:22:14 +0200

    Implemented frontend_master.py, frontend_master depends on command interpreter and output formatter

--- frontend/frontend_master.py
+++ frontend/frontend_master.py
@@ -1,27 +1,79 @@
 """
+
 :author: Jonathan Decker
 """
 
 
 import asyncio
 import logging
+from models.lookup_tables import next_step_lookup
+from models.errors import InvalidNextStepError
+from frontend.command_interpreter import interpret_command
+from frontend.output_formatter import format_payload
 
 logger = logging.getLogger("pb_logger")
 
 
-async def loop_back_dummy(forward_queue: asyncio.Queue, frontend_master_queue: asyncio.Queue):
+async def loop_back_dummy(forward_queue: asyncio.Queue, frontend_queue: asyncio.Queue):
     """
     :description: A dummy Coroutine that sends incoming queries back to the discord interface.
     :param forward_queue: The forward_queue which should be handled by the query_forwarder.
     :type forward_queue: asyncio.Queue
-    :param frontend_master_queue: The Queue that is handled by this Coroutine.
-    :type frontend_master_queue: asyncio.Queue
+    :param frontend_queue: The Queue that is handled by this Coroutine.
+    :type frontend_queue: asyncio.Queue
     :return: None
     :rtype: None
     """
     while True:
-        query = await frontend_master_queue.get()
+        query = await frontend_queue.get()
         query.forward_to = "discord"
         query.output_message = query.command
         forward_queue.put_nowait(query)
-        frontend_master_queue.task_done()
+        frontend_queue.task_done()
+
+
+async def frontend_loop(forward_queue: asyncio.Queue, frontend_queue: asyncio.Queue):
+
+    while True:
+        query = await frontend_queue.get()
+        try:
+            if query.next_step not in next_step_lookup:
+                raise InvalidNextStepError
+        except InvalidNextStepError:
+            logger.error(f"Invalid next_step in query {str(query)}, discarding query.")
+            del query
+            frontend_queue.task_done()
+            continue
+
+        # could use a dict here, instead of multiple elif checks but with only 3 there is not much point
+        if query.next_step == "interpret":
+            # call command interpreter with query
+            interpret_command(query)
+            # send query to forward_queue
+            forward_queue.put_nowait(query)
+
+        elif query.next_step == "format":
+            # call output formatter with query
+            format_payload(query)
+            # send query to forward_queue (should be self submit)
+            forward_queue.put_nowait(query)
+
+        elif query.next_step == "display":
+            pass
+            # check query context type, set forward_to and send to forward_queue
+            if query.context_type == "discord":
+                query.forward_to = "discord"
+                forward_queue.put_nowait(query)
+
+            else:
+                logger.error(f"Invalid context_type in query {str(query)}, discarding query.")
+                del query
+                frontend_queue.task_done()
+                continue
+
+        else:
+            # Next_step can't be handled by frontend, something went wrong
+            logger.error(f"Invalid control flow in frontend master for query {str(query)}, discarding query.")
+            del query
+
+        frontend_queue.task_done()
